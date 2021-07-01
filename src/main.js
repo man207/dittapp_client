8c0b3233b448838783118a4c165b763051f5bb5a
man207 <man207ej@gmail.com>
2021-07-01 14:16:36 +0430

    vuetify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import vuetify from './plugins/vuetify'
 
 
 axios.defaults.baseURL = 'http://localost:8085/'
@@ -26,5 +27,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
